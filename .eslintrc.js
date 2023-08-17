dd773c2f29e4f36fb815211ede64ab3ae60fd031
xlf60 <1902745266@qq.com>
2023-08-17 09:55:05 +0800

    第二次提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  extends: [require.resolve('@umijs/lint/dist/config/eslint')],
   globals: {
     page: true,
     REACT_APP_ENV: true,
   },
 };
+// module.exports = {
+//   extends: [require.resolve('@umijs/lint/dist/config/eslint')],
+//   globals: {
+//     page: true,
+//     REACT_APP_ENV: true,
+//   },
+// };
